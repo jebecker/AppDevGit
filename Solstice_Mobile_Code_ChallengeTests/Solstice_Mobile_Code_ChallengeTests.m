eccc478fbf7d391e5367e4d84172ef573077e6bd
Jayme Becker <beckerjayme@gmail.com>
2015-04-03 11:33:12 -0500

    iOS App Development
    General app dev

---
+++ Solstice_Mobile_Code_ChallengeTests/Solstice_Mobile_Code_ChallengeTests.m
@@ -0,0 +1,40 @@
+//
+//  Solstice_Mobile_Code_ChallengeTests.m
+//  Solstice_Mobile_Code_ChallengeTests
+//
+//  Created by Jayme Becker on 9/23/14.
+//  Copyright (c) 2014 Jayme Becker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Solstice_Mobile_Code_ChallengeTests : XCTestCase
+
+@end
+
+@implementation Solstice_Mobile_Code_ChallengeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
